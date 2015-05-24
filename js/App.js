b096fdcbc3d0dc45a2f9d9c4414c5e3eba678c44
egilsster <egilsster@gmail.com>
2015-05-24 15:47:48 +0000

    refactor

--- js/App.js
+++ js/App.js
@@ -2,15 +2,11 @@ angular.module('HomeApp', ['ngRoute']);
 
 angular.module('HomeApp').config(['$routeProvider', function ($routeProvider) {
     $routeProvider. 
-    when("/main", {
-        templateUrl: "templates/main.html",
-        controller: "MainController"
-    }).
-    when("/cv", {
+    when("/", {
         templateUrl: "templates/cv.html",
         controller: "CVController"
     }).
     otherwise({
-        redirectTo: "/main"
+        redirectTo: "/"
     });
 }]);
