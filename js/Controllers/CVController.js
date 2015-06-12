32a180b02287a793a3308ed4280a6a3f040aee5f
egilsster <egilsster@gmail.com>
2015-06-12 18:20:24 -0400

    fixed url

--- js/Controllers/CVController.js
+++ js/Controllers/CVController.js
@@ -59,7 +59,7 @@ angular.module('HomeApp').controller('CVController', ["$scope", "$rootScope", "$
         {
             workplace: "Reykjavík University",
             name: "Mini Mooshak",
-            url: "mooshak.spagli.io",
+            url: "http://mooshak.spagli.io/",
             date: "may 2015",
             location: "Reykjavík, Iceland",
             description: "A server written in Python 3.4 using the Flask microframework. It's a server that automatically grades solutions sent in by students. It's a project I made in a Python course. The real Mooshak is a system that Reykjavík University uses to grade solutions by students and gives feedback. The problem descriptions were taken from the real Mooshak and Kattis. See the projects GitHub page for more information."
