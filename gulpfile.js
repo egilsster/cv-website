096691fb87bb8c3e16b2c639d349053888430ef4
Egill Sveinbjörnsson <egillsveinbjorns@gmail.com>
2016-04-16 14:49:38 -0400

    Cleanup and restructure

--- gulpfile.js
+++ gulpfile.js
@@ -1,54 +1,27 @@
 var gulp = require('gulp'),
-	sass = require('gulp-ruby-sass'),
-	autoprefixer = require('gulp-autoprefixer'),
-	minifycss = require('gulp-minify-css'),
-	rename = require('gulp-rename'),
-	minifyHTML = require('gulp-minify-html');
+    sass = require('gulp-sass'),
+    minifycss = require('gulp-minify-css'),
+    rename = require('gulp-rename'),
+    minifyHTML = require('gulp-minify-html');
 
-gulp.task('express', function () {
-	var express = require('express');
-	var app = express();
-	app.use(require('connect-livereload')({port: 35729}));
-	app.use(express.static(__dirname));
-	app.listen(4000, '0.0.0.0');
-});
-
-var tinylr;
-gulp.task('livereload', function () {
-	tinylr = require('tiny-lr')();
-	tinylr.listen(35729);
-});
-
-function notifyLiveReload(event) {
-	var fileName = require('path').relative(__dirname, event.path);
-
-	tinylr.changed({
-		body: {
-			files: [fileName]
-		}
-	});
-}
-
-gulp.task('styles', function () {
-	return sass('sass', {style: 'expanded'})
-		.pipe(gulp.dest('css'))
-		.pipe(rename({suffix: '.min'}))
-		.pipe(minifycss())
-		.pipe(gulp.dest('css'));
+gulp.task('sass', function () {
+    gulp.src('./sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(minifycss())
+        .pipe(gulp.dest('./dist/css'));
 });
 
 gulp.task('watch', function () {
-	gulp.watch('sass/*.sass', ['styles']);
-	gulp.watch('*.html', notifyLiveReload);
-	gulp.watch('css/*.css', notifyLiveReload);
+    gulp.watch('sass/**/*.scss', ['sass']);
+    gulp.watch('haml/**/*.haml', ['haml']);
 });
 
-gulp.task('minify-html', function() {
-	return gulp.src('src/*.html')
-		.pipe(minifyHTML({ empty: true }))
-		.pipe(gulp.dest('./'));
+
+gulp.task('minify-html', function () {
+    gulp.src('src/*.html')
+        .pipe(minifyHTML({empty: true}))
+        .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('default', ['styles', 'minify-html', 'express', 'livereload', 'watch'], function () {
-
-});
+gulp.task('default', ['sass', 'minify-html', 'watch']);
