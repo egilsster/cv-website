3a656ea709eeb62c319e4d210c11b472ef392076
Egill Sveinbjörnsson <egillsveinbjorns@gmail.com>
2018-03-05 19:48:57 +0000

    Refreshing

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,19 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
-    minifycss = require('gulp-minify-css'),
-    rename = require('gulp-rename'),
-    minifyHTML = require('gulp-minify-html');
+    minifyCss = require('gulp-minify-css'),
+    rename = require('gulp-rename');
 
 gulp.task('sass', function () {
     gulp.src('./sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(rename({suffix: '.min'}))
-        .pipe(minifycss())
+        .pipe(minifyCss())
         .pipe(gulp.dest('./css'));
 });
 
 gulp.task('watch', function () {
     gulp.watch('sass/**/*.scss', ['sass']);
-    gulp.watch('src/**/*.html', ['minify-html']);
 });
 
 
-gulp.task('minify-html', function () {
-    gulp.src('src/*.html')
-        .pipe(minifyHTML({empty: true}))
-        .pipe(gulp.dest('./'));
-});
-
-gulp.task('default', ['sass', 'minify-html', 'watch']);
+gulp.task('default', ['sass', 'watch']);
