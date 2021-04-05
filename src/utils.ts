e0b34e5a09cda50c1d3a7a943b19ee0fcc5ada37
Egill Sveinbjörnsson <egillsveinbjorns@gmail.com>
2021-04-05 23:53:30 +0200

    fix: plural years/months

--- src/utils.ts
+++ src/utils.ts
@@ -8,12 +8,20 @@ export function formatDate(value: string | undefined): string {
 }
 
 export function getTimeRange(start: string, end: string | undefined): string {
+  function fmt(text: string, plural: boolean): string {
+    return text.concat(plural ? "s" : "");
+  }
+
   const { months, years } = intervalToDuration({
     start: new Date(start),
     end: end ? new Date(end) : new Date(),
   });
+  const ranges: string[] = [];
   if (years) {
-    return `${years} years, ${months} month(s)`;
+    ranges.push(`${years} ${fmt("year", years > 1)}`);
   }
-  return `${months} month(s)`;
+  if (months) {
+    ranges.push(`${months} ${fmt("month", months > 1)}`);
+  }
+  return ranges.join(", ");
 }
